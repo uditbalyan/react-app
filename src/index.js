caf7bb85e3be6b8de0320a87c74e66890b2d6968
Udit Balyan <udfan14@gmail.com>
2020-07-15 01:09:38 +0530

    subscribing store, forced update rerendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
 
-
-ReactDOM.render(<App />, document.getElementById('root')
-);
+const store =createStore(movies);
+console.log(store);
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 
